d74ad8568d8810f79565315483a6e280e85754cf
Cauê <cauegastaldi@outlook.com>
2022-10-27 11:29:41 -0300

    Added remove employee use case and made some changes in EmployeeDAO, SQLiteEmployeeDAO and UpdateEmployeeUseCase.

--- src/main/java/br/edu/ifsp/addthenewsoul/application/database/SQLiteEmployeeDAO.java
+++ src/main/java/br/edu/ifsp/addthenewsoul/application/database/SQLiteEmployeeDAO.java
@@ -1,9 +1,13 @@
 package br.edu.ifsp.addthenewsoul.application.database;
 
+import br.edu.ifsp.addthenewsoul.domain.entities.asset.Local;
 import br.edu.ifsp.addthenewsoul.domain.entities.employee.Employee;
+import br.edu.ifsp.addthenewsoul.domain.entities.employee.Role;
 import br.edu.ifsp.addthenewsoul.domain.usecases.employee.EmployeeDAO;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -11,14 +15,7 @@ import java.util.Optional;
 public class SQLiteEmployeeDAO implements EmployeeDAO {
 
     @Override
-    public Optional<Employee> findByRegistrationNumber(Integer registrationNumber) {
-        String sql = "SELECT * FROM Employee where registrationNumber = ?";
-
-        return Optional.empty();
-    }
-
-    @Override
-    public Integer add(Employee employee) {
+    public String add(Employee employee) {
         String sql = "INSERT INTO Employee(name, registrationNumber, hashPassword, email, phone, role) VALUES (?, ?, ?, ?, ?, ?)";
 
 
@@ -43,11 +40,52 @@ public class SQLiteEmployeeDAO implements EmployeeDAO {
     }
 
     @Override
-    public boolean delete(Integer key) {
+    public boolean delete(String registrationNumber) {
         String sql = "DELETE FROM Employee WHERE registrationNumber = ?";
 
+        try (PreparedStatement stmt = Database.createPreparedStatement(sql)) {
+            stmt.setString(1, registrationNumber);
+            stmt.execute();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         return false;
     }
 
+    @Override
+    public Optional<Employee> findByRegistrationNumber(String registrationNumber) {
+        String sql = "SELECT * FROM Employee where registrationNumber = ?";
+        Employee employee = null;
+        try (PreparedStatement stmt = Database.createPreparedStatement(sql)) {
+            stmt.setString(1, registrationNumber);
+
+            ResultSet resultSet = stmt.executeQuery();
+            if (resultSet.next()) {
+                employee = resultSetToEntity(resultSet);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return Optional.ofNullable(employee);
+    }
+
+    private Employee resultSetToEntity(ResultSet rs) throws SQLException {
+        Role role;
+        if (rs.getString("role").equals("INVENTORY MANAGER"))
+            role = Role.INVENTORY_MANAGER;
+        else
+            role = Role.EXECUTOR;
+
+        return new Employee(
+                            rs.getString("name"),
+                            rs.getString("registrationNumber"),
+                            rs.getString("hashPassword"),
+                            rs.getString("email"),
+                            rs.getString("phone"),
+                            role
+        );
+    }
 }
