8bf624bcf7b236f72f63e704a7d225d756324ddb
isarnf <isarnf@gmail.com>
2022-10-27 11:20:28 -0300

    correction InventoryAsset constructor

--- src/main/java/br/edu/ifsp/addthenewsoul/domain/entities/inventory/InventoryAsset.java
+++ src/main/java/br/edu/ifsp/addthenewsoul/domain/entities/inventory/InventoryAsset.java
@@ -1,14 +1,15 @@
 package br.edu.ifsp.addthenewsoul.domain.entities.inventory;
 
 import br.edu.ifsp.addthenewsoul.domain.entities.asset.Asset;
+import br.edu.ifsp.addthenewsoul.domain.entities.asset.Local;
 import br.edu.ifsp.addthenewsoul.domain.entities.employee.Employee;
 
 
 public class InventoryAsset extends Asset {
     private Employee executor;
 
-    public InventoryAsset(int id, String description, Employee employeeInCharge, double value, String damage, Employee executor) {
-        super(id, description, employeeInCharge, value, damage);
+    public InventoryAsset(int id, String description, Employee employeeInCharge, double value, String damage, Employee executor, Local location) {
+        super(id, description, employeeInCharge, value, damage, location);
         this.executor = executor;
     }
 
