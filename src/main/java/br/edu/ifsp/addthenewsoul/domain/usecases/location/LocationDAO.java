c23cb5aca3365265a8295f0c8d993ccf6e1deb78
Vinicius Ricardo <viniciusnoli2016@gmail.com>
2022-10-28 14:43:52 -0300

    use case delete location ready

--- src/main/java/br/edu/ifsp/addthenewsoul/domain/usecases/location/LocationDAO.java
+++ src/main/java/br/edu/ifsp/addthenewsoul/domain/usecases/location/LocationDAO.java
@@ -1,11 +1,16 @@
 package br.edu.ifsp.addthenewsoul.domain.usecases.location;
+import br.edu.ifsp.addthenewsoul.domain.entities.asset.Asset;
 import br.edu.ifsp.addthenewsoul.domain.usecases.utils.DAO;
 import br.edu.ifsp.addthenewsoul.domain.entities.asset.Location;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LocationDAO extends DAO<Location, Integer> {
 
     Optional<Location> findById(Integer id);
+
     Optional<Location> findByLocation(Integer number, String section);
+
+    boolean haveAssets (List<Asset> assets, Location location);
 }
