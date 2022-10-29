e1244968748655c7c0570b61750c5b08e491fe88
isarnf <isarnf@gmail.com>
2022-10-28 23:38:55 -0300

    addition of FindEmployeeUseCase

---
+++ src/main/java/br/edu/ifsp/addthenewsoul/domain/usecases/employee/FindEmployeeUseCase.java
@@ -0,0 +1,26 @@
+package br.edu.ifsp.addthenewsoul.domain.usecases.employee;
+
+import br.edu.ifsp.addthenewsoul.domain.entities.employee.Employee;
+import br.edu.ifsp.addthenewsoul.domain.usecases.utils.EntityNotFoundException;
+import br.edu.ifsp.addthenewsoul.domain.usecases.utils.Validator;
+
+import java.util.List;
+import java.util.Optional;
+
+public class FindEmployeeUseCase {
+    private EmployeeDAO employeeDAO;
+
+    public FindEmployeeUseCase(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    public Optional<Employee> findOne (String  registrationNumber) {
+        if (Validator.nullOrEmpty(registrationNumber))
+            throw new EntityNotFoundException("Employee not found. Registration number cannot be null or empty.");
+        return employeeDAO.findByRegistrationNumber(registrationNumber);
+    }
+
+    public List<Employee> findAll () {
+        return employeeDAO.findAll();
+    }
+}
