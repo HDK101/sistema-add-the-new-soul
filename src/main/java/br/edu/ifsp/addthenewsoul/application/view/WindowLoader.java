7a4480201548ab1508b73138180eec886ded00ae
Vinicius Noli <viniciusnoli2016@gmail.com>
2022-11-24 10:48:18 -0300

    correções da classe WindowLoarder

---
+++ src/main/java/br/edu/ifsp/addthenewsoul/application/view/WindowLoader.java
@@ -0,0 +1,43 @@
+package br.edu.ifsp.addthenewsoul.application.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class WindowLoader extends Application {
+
+    private static Scene scene;
+    private static Object controller;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("hello-views"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Parent parent = fxmlLoader.load(WindowLoader.class.getResource(fxml + ".fxml").openStream());
+        controller = fxmlLoader.getController();
+        return parent;
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    public static Object getController() {
+        return controller;
+    }
+
+}
