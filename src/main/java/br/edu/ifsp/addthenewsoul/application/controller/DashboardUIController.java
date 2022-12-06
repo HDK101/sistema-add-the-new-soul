0adaee8421c4e91df8595f906144f666b813058e
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2022-12-05 23:00:58 -0300

    dashboard mais bonito

--- src/main/java/br/edu/ifsp/addthenewsoul/application/controller/DashboardUIController.java
+++ src/main/java/br/edu/ifsp/addthenewsoul/application/controller/DashboardUIController.java
@@ -1,11 +1,21 @@
 package br.edu.ifsp.addthenewsoul.application.controller;
 
 import br.edu.ifsp.addthenewsoul.application.view.WindowLoader;
+import br.edu.ifsp.addthenewsoul.domain.usecases.utils.Session;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 import java.io.IOException;
 
 public class DashboardUIController {
+    public Label greetingsText;
+
+    @FXML
+    public void initialize() {
+        greetingsText.setText("Olá, " + Session.getInstance().getLoggedUser().getName());
+    }
+
     public void goToEmployees(ActionEvent actionEvent) {
         try {
             WindowLoader.setRoot("EmployeeManagementUI");
@@ -33,6 +43,24 @@ public class DashboardUIController {
     public void goToInventories(ActionEvent actionEvent) {
         try {
             WindowLoader.setRoot("InventoryManagementUI");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void goToReports(ActionEvent actionEvent) {
+        try {
+            Session.logout();
+            WindowLoader.setRoot("ReportManagementUI");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void logout(ActionEvent actionEvent) {
+        try {
+            Session.logout();
+            WindowLoader.setRoot("LoginUI");
         } catch (IOException e) {
             e.printStackTrace();
         }
